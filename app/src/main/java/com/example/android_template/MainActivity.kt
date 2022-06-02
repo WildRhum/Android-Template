c0f4751af2e6bfcbae7b11e06777d1217aaafcb8
WildRhum <maximoukyon@gmail.com>
2022-06-02 17:47:50 +0200

    Compose-Navigation : - Implementation of the compose navigation using Scafold + Package - Suppression of unused files (xml + navigation and rename fragment) - Updating gradle - Updating lib - Enhance file and directory management - Adding materialTheme

--- app/src/main/java/com/example/android_template/MainActivity.kt
+++ app/src/main/java/com/example/android_template/MainActivity.kt
@@ -1,35 +1,79 @@
 package com.example.android_template
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.android_template.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.*
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.android_template.ui.Screen
+import com.example.android_template.ui.dashboard.Dashboard
+import com.example.android_template.ui.home.Home
+import com.example.android_template.ui.notifications.Notifications
+import com.example.android_template.ui.theme.AndroidTemplateTheme
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+@ExperimentalMaterial3Api
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setContent {
+            AndroidTemplateTheme {
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = {
+                        NavigationBar {
+                            val navBackStackEntry by navController.currentBackStackEntryAsState()
+                            val currentDestination = navBackStackEntry?.destination
+                            getScreens().forEach { screen ->
+                                NavigationBarItem(
+                                    icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                                    label = { Text(stringResource(screen.resourceId)) },
+                                    selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                                    onClick = {
+                                        navController.navigate(screen.route) {
+                                            // Pop up to the start destination of the graph to
+                                            // avoid building up a large stack of destinations
+                                            // on the back stack as users select items
+                                            popUpTo(navController.graph.findStartDestination().id) {
+                                                saveState = true
+                                            }
+                                            // Avoid multiple copies of the same destination when
+                                            // reselecting the same item
+                                            launchSingleTop = true
+                                            // Restore state when reselecting a previously selected item
+                                            restoreState = true
+                                        }
+                                    }
+                                )
+                            }
+                        }
+                    }
+                ) { innerPadding ->
+                    NavHost(
+                        navController,
+                        startDestination = Screen.Home.route,
+                        Modifier.padding(innerPadding)
+                    ) {
+                        composable(Screen.Home.route) { Home(navController) }
+                        composable(Screen.Dashboard.route) { Dashboard(navController) }
+                        composable(Screen.Notifications.route) { Notifications(navController) }
+                    }
+                }
+            }
+        }
     }
+}
+
+fun getScreens() : List<Screen> {
+    return Screen::class.nestedClasses.map { it.objectInstance as Screen }
 }
